446029ea5e7daea921cfe78b9a2b01c4dfab3855
Lucas Templeton <templetl99@gmail.com>
2022-01-03 16:55:11 -0600

    tables

--- backend/db/migrate/20220103215857_create_reviews.rb
+++ backend/db/migrate/20220103215857_create_reviews.rb
@@ -1,4 +1,9 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
+    create_table :reviews do |t|
+      t.integer :rating_number
+      t.string :rating_text
+      t.datetime :created_at
+    end
   end
 end
