309d0e204d7243bd5bdbcb80a541a3e4a966ac47
ivancolmenares <ivancolmenares@live.com>
2022-01-03 16:51:15 -0600

    changes

--- backend/db/migrate/20220103215755_create_users.rb
+++ backend/db/migrate/20220103215755_create_users.rb
@@ -1,4 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
+    create_table :users do |t|
+      t.integer :post_id
+      t.integer :review_id
+      t.string :name
+
+    end
+    
+
   end
 end
