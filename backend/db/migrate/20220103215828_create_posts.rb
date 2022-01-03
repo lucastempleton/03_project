edcf022108ffd2a3f12fe68857e501dd37526b64
Lucas Templeton <templetl99@gmail.com>
2022-01-03 16:59:21 -0600

    3

--- backend/db/migrate/20220103215828_create_posts.rb
+++ backend/db/migrate/20220103215828_create_posts.rb
@@ -3,6 +3,10 @@ class CreatePosts < ActiveRecord::Migration[6.1]
     create_table do |t|
       t.integer :post_id
       t.string  :title
+      t.string :img_url
+      t.string :description
+      t.string :location
+      t.string :phone_number
       t.datetime :posted_at
   end
 end
