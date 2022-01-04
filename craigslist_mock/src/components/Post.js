17429c45cbccdd6e0708d9f8513243a638129491
Lucas Templeton <templetl99@gmail.com>
2022-01-04 10:28:37 -0600

    complete

--- craigslist_mock/src/components/Post.js
+++ craigslist_mock/src/components/Post.js
@@ -1,13 +1,28 @@
 import { useState, useEffect } from 'react';
 import ChooseCategory from "./sub-components/ChooseCategory"
+import PostingDetails from "./sub-components/PostingDetails"
 
 export default function Post (){
+
 
     return(
         <div>
             <form>
                 <div className="category">Category</div>
                 <ChooseCategory />
+                <form>
+                    <p>posting title</p>
+                    <input type="text"></input>
+                    <p>price</p>
+                    <input></input>
+                    <p>city or neighborhood</p>
+                    <input></input>
+                    <p>postal code</p>
+                    <input></input>
+                    <p>description</p>
+                    <input></input>
+                    <PostingDetails/>
+                </form>
             </form>
         </div>
     )
---
+++ craigslist_mock/src/components/sub-components/PostingDetails.js
@@ -0,0 +1,15 @@
+
+export default function PostingDetails(){
+    return(
+        <div>
+            <h3>posting details</h3>
+                    <p>make / manufacturer</p>
+                    <input></input>
+                    <p>model name / number</p>
+                    <input></input>
+                    <p>size / dimensions</p>
+                    <input></input>
+                    <p>condition</p>
+        </div>
+    )
+}
