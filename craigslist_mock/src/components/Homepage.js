4e31a75367fffc1cac9797984a7ea9d3f1a817c1
ivancolmenares <ivancolmenares@live.com>
2022-01-04 12:04:06 -0600

    ivan

--- craigslist_mock/src/components/Homepage.js
+++ craigslist_mock/src/components/Homepage.js
@@ -1,8 +1,17 @@
 export default function Homepage(){
-    const categories = ["Antiques", "Appliances", "Arts/Crafts", "Sporting"]
+    const categories = ["Antiques", "Appliances", "Arts/Crafts", "Sporting Vehicles", "Auto Parts", "Auto Wheel/Tires", "Aviation", "Baby/Kid", "Barter", "Bicycyle Parts", "Boat Parts", "Books/Magazines", "Business/Commercial", "Cars/Trucks", "CDs/DVDs/VHS", "Cell Phones", "Clothing/Accesories", "Collectibles", "Computer Parts", "Computers", "Electronics", "Farm/Garden", "Free Stuff", "Furniture", "Garage/Moving Sale", "General Items", "Health/Beauty", "Heavy Equipment", "Household Items", "Jewelry", "Materials", "Motorcycles Parts", "Motorcycles/ Scooters", "Musical Instruments", "Photo/Video", "RVS", "Sporting Goods", "Tickets", "Tools", "Toys/Games", "Trailers", "Video Gaming", "Wanted"]
     return (
         <div>
-
+            <h1>CraigslistMock</h1>
+            <ul id="nav"> 
+        	<li><span>Post</span></li> 
+            <li><span>Sell</span></li> 
+            <li><span>Contact</span></li> 
+            </ul> 
+            <button type="button">Log in</button>           
+            <h2> Buy or sell easy</h2>
         </div>
+    function   
     )
+
 }
